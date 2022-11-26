ef8e360378f5d4b2026c32cc18fe9be43ea4e61a
lu2ep <lu2ep00@gmail.com>
2022-11-26 15:56:07 -0300

    Se agrega el proyecto URL con API

---
+++ src/main/java/com/APIlogin/APILOGIN/ApiloginApplication.java
@@ -0,0 +1,13 @@
+package com.APIlogin.APILOGIN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiloginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/APIlogin/APILOGIN/ApiloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.APIlogin.APILOGIN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
